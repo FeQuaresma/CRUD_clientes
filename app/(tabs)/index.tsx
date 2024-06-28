4e4119b23420fceae95210bfe5322bb6a0862e66
FeQuaresma <felipequada@gmail.com>
2024-06-27 23:20:48 -0300

    Form.map on index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,58 +10,110 @@ import {
 import { Dropdown } from "react-native-element-dropdown";
 
 export default function HomeScreen() {
-  const [nome, setNome] = useState("");
-  const [genero, setGenero] = useState("");
-  const [idade, setIdade] = useState("");
-  const lista:any = [];
+  // const [nome, setNome] = useState("");
+  // const [genero, setGenero] = useState("");
+  // const [idade, setIdade] = useState("");
+  // const lista:any = [];
 
-  const data = [
-    { label: "homem", value: "homem" },
-    { label: "mulher", value: "mulher" },
-    { label: "outro", value: "outro" },
+  // const data = [
+  //   { label: "homem", value: "homem" },
+  //   { label: "mulher", value: "mulher" },
+  //   { label: "outro", value: "outro" },
+  // ];
+
+  // const submitForm = () => {
+  //   lista.push({ Nome: nome, Gênero: genero, Idade: idade });
+  //   setNome("");
+  //   setGenero("");
+  //   setIdade("");
+  //   console.log(lista);
+  // };
+
+  // return (
+  //   <ScrollView contentContainerStyle={styles.container}>
+  //     <View>
+  //       <Text style={styles.inputLabel}>Nome</Text>
+  //       <TextInput
+  //         style={styles.input}
+  //         value={nome}
+  //         onChangeText={(e) => setNome(e)}
+  //       />
+  //     </View>
+  //     <View>
+  //       <Text style={styles.inputLabel}>Gênero</Text>
+  //       <Dropdown
+  //         style={styles.input}
+  //         data={data}
+  //         labelField={"label"}
+  //         valueField={"value"}
+  //         value={genero}
+  //         onChange={(e) => setGenero(e.value)}
+  //       />
+  //     </View>
+  //     <View>
+  //       <Text style={styles.inputLabel}>Idade</Text>
+  //       <TextInput
+  //         style={styles.input}
+  //         value={idade}
+  //         onChangeText={(e) => setIdade(e)}
+  //         maxLength={2}
+  //         inputMode="numeric"
+  //       />
+  //       <Button title="Submit" onPress={submitForm}/>
+  //     </View>
+  //     <View></View>
+  //   </ScrollView>
+  // );
+
+  const formParam: any = [
+    { label: "Nome", inputType: "text", dataType: "string", value: "" },
+    {
+      label: "Gênero",
+      inputType: "select",
+      dataType: [
+        { label: "homem", value: "homem" },
+        { label: "mulher", value: "mulher" },
+        { label: "outro", value: "outro" },
+      ], value: ""
+    },
+    { label: "Idade", inputType: "text", dataType: "number", value: "" },
+    { label: "CNPJ", inputType: "text", dataType: "number", value: "" },
   ];
-
-  const submitForm = () => {
-    lista.push({ Nome: nome, Gênero: genero, Idade: idade });
-    setNome("");
-    setGenero("");
-    setIdade("");
-    console.log(lista);
-  };
+  const [form, setForm] = useState(formParam);
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <View>
-        <Text style={styles.inputLabel}>Nome</Text>
-        <TextInput
-          style={styles.input}
-          value={nome}
-          onChangeText={(e) => setNome(e)}
-        />
-      </View>
-      <View>
-        <Text style={styles.inputLabel}>Gênero</Text>
-        <Dropdown
-          style={styles.input}
-          data={data}
-          labelField={"label"}
-          valueField={"value"}
-          value={genero}
-          onChange={(e) => setGenero(e.value)}
-        />
-      </View>
-      <View>
-        <Text style={styles.inputLabel}>Idade</Text>
-        <TextInput
-          style={styles.input}
-          value={idade}
-          onChangeText={(e) => setIdade(e)}
-          maxLength={2}
-          inputMode="numeric"
-        />
-        <Button title="Submit" onPress={submitForm}/>
-      </View>
-      <View></View>
+      {form.map((item:any, index:any) => (
+        <View key={index}>
+          <Text style={styles.inputLabel}>{item.label}</Text>
+          {item.inputType === "text" ? (
+            <TextInput
+              style={styles.input}
+              value={item.value}
+              onChangeText={(text) => {
+                const newForm = [...form];
+                newForm[index].value = text;
+                setForm(newForm);
+              }}
+              keyboardType={item.dataType === "number" ? "numeric" : "default"}
+            />
+          ) : (
+            <Dropdown
+              style={styles.input}
+              data={item.dataType}
+              labelField="label"
+              valueField="value"
+              value={item.value}
+              onChange={(e) => {
+                const newForm = [...form];
+                newForm[index].value = e.value;
+                setForm(newForm);
+              }}
+            />
+          )}
+        </View>
+      ))}
+      <Button title="Submit" onPress={() => form.map((value:any)=>console.log(value.value))}/>
     </ScrollView>
   );
 }

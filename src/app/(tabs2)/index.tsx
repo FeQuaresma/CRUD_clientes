1fe51c5bad04cee970216f87fa62aef63c0a82e5
FeQuaresma <felipequada@gmail.com>
2024-09-02 16:28:03 -0300

    New Dynamic Function Method

--- src/app/(tabs2)/index.tsx
+++ src/app/(tabs2)/index.tsx
@@ -1,28 +1,66 @@
-import { Pressable, ScrollView, Text, View } from "react-native";
+import { Pressable, ScrollView, Text, Alert } from "react-native";
 import * as React from "react";
+import { validateCPF } from "@/src/functions";
 
-export default function Home() {
-  const json = {
-    functionCode: "function showAlert() { Alert.alert('Hello from JSON!'); }",
+function index({ e }: any) {
+  <Home {...e} />;
+}
+
+export default function Home({ globalFunctions }: any) {
+  interface Json {
+    functionCode: string;
+    functionVar?: string;
+    functionValue: string;
+    importedFunc: { import: string; from: string };
+  }
+
+  // const json: Json = {
+  //   functionCode: "a((prev) => ({...prev, functionValue: b}));",
+  //   functionVar: "teste Var",
+  //   functionValue: "teste Value",
+  //   importedFunc: { import: "setJsonState", from: "local" },
+  // };
+
+  const json: Json = {
+    functionCode: "a.alert('Olá Mundo 2')",
+    // functionVar: "Olá Mundo",
+    functionValue: "teste Value",
     importedFunc: { import: "Alert", from: "react-native" },
   };
-  // Crie um mapeamento de strings para módulos
-  const moduleMap: { [key: string]: () => Promise<any> } = {
-    "react-native": () => import("react-native"),
+
+  const [jsonState, setJsonState] = React.useState(json);
+
+  const localFunctions = {
+    setJsonState,
   };
 
-  async function executeFunction() {
-    const { import: importName, from: importSource } = json.importedFunc;
+  const moduleMap: { [key: string]: () => Promise<any> | any } = {
+    "react-native": () => import("react-native"),
+    local: () => localFunctions,
+  };
+
+  async function executeFunction(jsonImported: Json) {
+    const { import: importName, from: importSource } =
+      jsonImported.importedFunc;
 
     const importedModule = await moduleMap[importSource]();
 
-    const importedFunc = importedModule[importName];
+    const importedFunc = importedModule
+      ? importedModule[importName]
+      : undefined;
 
-    const func = new Function(importName, "return " + json.functionCode)(
-      importedFunc
-    );
+    if (!importedFunc) {
+      console.error(`Function ${importName} not found in ${importSource}`);
+      return;
+    }
 
-    func();
+    const func = new Function("a", "b", jsonImported.functionCode);
+
+    if (jsonImported.functionVar) {
+      func(importedFunc, jsonImported.functionVar);
+    } else {
+      func(importedFunc);
+    }
   }
 
   return (
@@ -33,9 +71,15 @@ export default function Home() {
         alignItems: "center",
       }}
     >
-      <Pressable onPress={() => executeFunction()}>
-        <Text>Func</Text>
+      <Pressable onPress={() => executeFunction(jsonState)}>
+        <Text style={{ fontSize: 30 }}>Func</Text>
       </Pressable>
+
+      <Pressable onPress={() => setJsonState(json)}>
+        <Text style={{ fontSize: 30 }}>Go back</Text>
+      </Pressable>
+
+      <Text style={{ fontSize: 30 }}>{jsonState.functionValue}</Text>
     </ScrollView>
   );
 }
